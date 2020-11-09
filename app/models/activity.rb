58948e6ca25cfc876ebe8787a01b30e6e620c58a
Daniel  Shnaider <danielshnaider@MacBook-Pro.local>
2020-11-09 17:36:09 -0500

    Minor syntax changes to Activity Model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,5 @@
 class Activity < ApplicationRecord
-  validates :type, inclusion: { in: ["Email", "Phone Call", "Social Media Interaction", "In person visit", "Just a Note"] }
+  validates :type, inclusion: { in: ["Email", "Phone Call", "Social Media Interaction", "In person visit", "Just a Note"] }, presence: true
   validates :title, prescence: true
   validates :notes, presence: true
 end
